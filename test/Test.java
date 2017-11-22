32950a3544c3b5b24ecc90d9e26e13c4bfa12ab8
mahff <33895997+mahff@users.noreply.github.com>
2017-11-22 11:37:28 +0100

    Update Test.java

--- test/Test.java
+++ test/Test.java
@@ -6,8 +6,10 @@ import source.*;
 public class Test {
 	public static void main(String[] args) {
 		SitemapGenerator sitemap = new SitemapGenerator("http://northkorea.nk");
+		FileData file = new FileData(new File("sitemap.xml"));
 		try {
 			sitemap.execute("/home/sevenmoor/promenade_fr.d");
+			file.readFile("/home/sevenmoor/promenade_fr.d");
 		} catch (IOException e) {
 			System.err.println(e.getMessage());
 		}
