8272979db9fb0b0dfa6c85770db5a390ebac1c60
SevenMoor <33595291+SevenMoor@users.noreply.github.com>
2017-11-23 11:39:11 +0100

    Optimized the Structure & Fixed the readFile
    - Fixed the Regex usage and extended it to recognize h1
    - Made the reader case insensitive
    - Deleted getFile
    - Reduced the toString
    - Initialized location in the Constructor

--- source/FileData.java
+++ source/FileData.java
@@ -1,4 +1,4 @@
-package Source;
+package source;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -11,69 +11,49 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
-public class FileData {
+public class FileData{
+	
 	private File file; 
 	private SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd");
 	private ArrayList<String> keywords = new ArrayList<String>();
 	private String location; 
 	
-	public FileData(File currentFile) {
+	public FileData(File currentFile){
 		super();
-		this.file = currentFile;
-		
-		
+		this.location = currentFile.getAbsolutePath();
+		this.file = currentFile;	
 	}
-	
 	public void readFile(String fileName) throws IOException{
-		
-		BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
-		 
-		  StringBuffer stringBuffer = new StringBuffer();
-		  String line = null;
-		 
-		  while((line = bufferedReader.readLine())!=null){
-		 
-			  Pattern p = Pattern.compile("<title>(\\W+)</title>");
-			  Matcher m = p.matcher(stringBuffer);
-			  if (m.find()) {
-				  String codeGroup = m.group(1);
-				  System.out.format(codeGroup);
-			  }
-		  }
-		   
-		  System.out.println(stringBuffer);
-	}
-     
-	
-
-	public File getFile() {
-		return file;
+		BufferedReader reader = new BufferedReader(new FileReader(fileName));
+		String line = null;
+		while((line = reader.readLine().toLowerCase())!=null){
+			Pattern title = Pattern.compile("(<title>(\\W+)</title>|<h1>(\\W+)</h1>)");
+			Matcher matcher = title.matcher(new StringBuffer());
+			if (matcher.find()) {
+				for(int i=1;i<=matcher.groupCount();i++){  
+					String codeGroup = matcher.group(i);
+					String[] keys = codeGroup.split(" ");
+					for(String key : keys){
+						keywords.add(key);
+					}
+				}
+			}
+		}
+		reader.close();
 	}
 
-	public String getDate() {
+	public String getDate(){
 		return date.format(file.lastModified());
 	}
-
-
-	public ArrayList<String> getKeywords() {
+	public ArrayList<String> getKeywords(){
 		return keywords;
 	}
-
-
-	public String getVocation() {
+	public String getLocation(){
 		return location;
 	}
 
 	@Override
 	public String toString() {
-		return "FileData [file=" + file + ", date=" + date + ", keywords=" + keywords + ", location=" + location + "]";
+		return "FileData[date="+date+",keywords="+keywords+", location="+location +"]";
 	}
-
-	
-	
-	
-	
-	
-	
-
 }
