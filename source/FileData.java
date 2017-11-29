aa56eeb4ca229a4d364e013b65d8b78dfff10241
SevenMoor <33595291+SevenMoor@users.noreply.github.com>
2017-11-29 10:54:45 +0100

    Updated to the final version
    - Optimized the split function
    - Fixed the grouping problems
    - Fixed the case
    - Commented the code where needed

--- source/FileData.java
+++ source/FileData.java
@@ -10,50 +10,72 @@ import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
-public class FileData{
-	
-	private File file; 
+public class FileData {
+	private File file;
 	private SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd");
 	private ArrayList<String> keywords = new ArrayList<String>();
-	private String location; 
-	
-	public FileData(File currentFile){
+	private String location;
+
+	public FileData(File currentFile) {
 		super();
 		this.location = currentFile.getAbsolutePath();
-		this.file = currentFile;	
+		this.file = currentFile;
 	}
-	public void readFile(String fileName) throws IOException{
-		BufferedReader reader = new BufferedReader(new FileReader(fileName));
+
+	public void readFile() throws IOException {
+		BufferedReader reader = new BufferedReader(new FileReader(file));
 		String line = null;
-		while((line = reader.readLine().toLowerCase())!=null){
-			Pattern title = Pattern.compile("(<title>(\\W+)</title>|<h1>(\\W+)</h1>)");
-			Matcher matcher = title.matcher(new StringBuffer());
-			if (matcher.find()) {
-				for(int i=1;i<=matcher.groupCount();i++){  
-					String codeGroup = matcher.group(i);
-					String[] keys = codeGroup.split(" ");
-					for(String key : keys){
+		while ((line = reader.readLine()) != null) {
+			
+			//If we encounter a h1 or title tag...
+			Pattern title = Pattern.compile("(<h1>(.*?)</h1>|<title>(.*?)</title>)", Pattern.CASE_INSENSITIVE);
+			Matcher matcher = title.matcher(line);
+			
+			while (matcher.find()) {
+				//First process the h1 matches
+				String codeGroup = matcher.group(2);
+				if (codeGroup != null) {
+					String[] keys = codeGroup.split(" |&nbsp;|:|\"|-|\\(|\\)|\\?|!|\\.|\\n|\\t|;|,|\'"); //Split according to these characters
+					for (String key : keys)
 						keywords.add(key);
+				}
+				
+				//Then the title matches
+				codeGroup = matcher.group(3);
+				if (codeGroup != null) {
+					String[] keys = codeGroup.split(" |&nbsp;|:|\"|-|\\(|\\)|\\?|!|\\.|\\n|\\t|;|,|\'");
+					for (String key : keys)
+						keywords.add(key);
+				}
+				
+				//And remove empty entries
+				String empty = new String();
+				for (int i = 0; i < keywords.size(); i++) {
+					if (keywords.get(i).equals(empty)){
+						keywords.remove(i);
+						i--;
 					}
 				}
+
 			}
 		}
 		reader.close();
 	}
 
-	public String getDate(){
+	public String getDate() {
 		return date.format(file.lastModified());
 	}
-	public ArrayList<String> getKeywords(){
+
+	public ArrayList<String> getKeywords() {
 		return keywords;
 	}
-	public String getLocation(){
+
+	public String getLocation() {
 		return location;
 	}
 
 	@Override
 	public String toString() {
-		return "FileData[date="+date+",keywords="+keywords+", location="+location +"]";
+		return "FileData[date=" + date + ",keywords=" + keywords + ", location=" + location + "]";
 	}
 }
