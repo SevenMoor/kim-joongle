eb03d6331215cf724385f6011acb4655c89b5176
mahff <33895997+mahff@users.noreply.github.com>
2017-12-23 22:45:20 +0100

    Adaptation for windows

--- source/BrowseFile.java
+++ source/BrowseFile.java
@@ -7,20 +7,19 @@ import java.io.IOException;
 
 public class BrowseFile {
 
-
 	public BrowseFile() {
 	}
 
 	public void iterate(String currentDirectory, BufferedWriter writer) throws IOException {
 		File currentFile = new File(currentDirectory);
-		if (currentFile.isDirectory()){
+		if (currentFile.isDirectory()) {
 			String[] content = currentFile.list();
 			for (int i = 0; i < content.length; i++) {
 				String subDirectory = currentDirectory + "/" + content[i];
 				File subFile = new File(subDirectory);
 				if (subFile.isDirectory())
 					iterate(subDirectory, writer);
-				if (subFile.isFile()){
+				if (subFile.isFile()) {
 					if (subDirectory.endsWith(".html")) {
 						FileData file = new FileData(new File(subDirectory));
 						file.readFile();
@@ -33,7 +32,7 @@ public class BrowseFile {
 
 	public void execute(String currentDirectory) throws IOException {
 		BufferedWriter writer = new BufferedWriter(new FileWriter("keywords.xml"));
-		writer.write("<?xml version=\"1.0\" encoding=\"utf-8\" ?>\n\t<root>\n");
+		writer.write("<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n\t<root>\n");
 		iterate(currentDirectory, writer);
 		writer.write("\t</root>");
 		writer.close();
