da2cfd9ffdb991f49adfbfbbe840f358451081aa
mahff <33895997+mahff@users.noreply.github.com>
2017-12-27 13:46:04 +0100

    Update KimGraphic.java

--- program/KimGraphic.java
+++ program/KimGraphic.java
@@ -6,6 +6,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 
 import javafx.application.Application;
@@ -14,6 +15,7 @@ import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
@@ -24,10 +26,13 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.FlowPane;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import source.BrowseFile;
 import source.FileData;
 import source.SearchMatcher;
+import source.SitemapGenerator;
 
 public class KimGraphic extends Application {
 
@@ -35,10 +40,12 @@ public class KimGraphic extends Application {
 		Application.launch(args);
 	}
 
-	public void start(Stage stage) {
+	public void start(final Stage stage) {
+		final DirectoryChooser directoryChooser = new DirectoryChooser();
 		ImageView logo = null;
 		stage.setTitle("Kim Joongle");
-		FlowPane root = new FlowPane();
+		final FlowPane root = new FlowPane();
+		final Scene scene = new Scene(root, 1200, 600);
 
 		try {
 			logo = new ImageView(new Image(new FileInputStream("kim.png"), 300, 150, true, true));
@@ -48,6 +55,7 @@ public class KimGraphic extends Application {
 		}
 
 		final TextArea textArea = new TextArea();
+		final Label searchArea = new Label();
 
 		ScrollPane scrollPane = new ScrollPane();
 		scrollPane.setContent(textArea);
@@ -56,14 +64,13 @@ public class KimGraphic extends Application {
 		scrollPane.setMinHeight(300);
 
 		// Create the text fields
-		TextField researchField = new TextField();
+		final TextField researchField = new TextField();
 		researchField.setMinWidth(500);
 
 		// Create the button
 		Button searchButton = new Button("Search");
 		searchButton.setPrefSize(150, 50);
 		searchButton.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
 			public void handle(ActionEvent event) {
 				SearchMatcher matcher = new SearchMatcher();
 				try {
@@ -75,8 +82,9 @@ public class KimGraphic extends Application {
 					for (String match : matches) {
 						results.add(new FileData(new File(match)));
 					}
+					searchArea.setText("\t\t\t\t\t" + researchField.getText().toUpperCase() + " \n");
 					for (FileData result : results) {
-						textArea.appendText(result + "\n");
+						textArea.appendText("-----------******----------\n" + result + "\n\n");
 					}
 
 				} catch (ClassNotFoundException e) {
@@ -88,11 +96,10 @@ public class KimGraphic extends Application {
 			}
 		});
 		// Clear
-		Button buttonClear = new Button("Clear");
+		final Button buttonClear = new Button("Clear");
 		buttonClear.setPrefSize(100, 50);
 		buttonClear.setOnAction(new EventHandler<ActionEvent>() {
 
-			@Override
 			public void handle(ActionEvent event) {
 				textArea.clear();
 			}
@@ -108,9 +115,9 @@ public class KimGraphic extends Application {
 		Menu helpMenu = new Menu("Help");
 
 		// Create MenuItems
-		MenuItem newItem = new MenuItem("New");
+		MenuItem nightMode = new MenuItem("Mode Night");
 		MenuItem exitItem = new MenuItem("Exit");
-
+		MenuItem selectDirectory = new MenuItem("Select Directory");
 		MenuItem xmlOpen = new MenuItem("Open Xml Files");
 		MenuItem readMe = new MenuItem("Read ME");
 
@@ -118,6 +125,36 @@ public class KimGraphic extends Application {
 		exitItem.setAccelerator(KeyCombination.keyCombination("Ctrl+W"));
 		xmlOpen.setAccelerator(KeyCombination.keyCombination("Ctrl+X"));
 		readMe.setAccelerator(KeyCombination.keyCombination("Ctrl+M"));
+		nightMode.setAccelerator(KeyCombination.keyCombination("Ctrl+L"));
+		selectDirectory.setAccelerator(KeyCombination.keyCombination("Ctrl+K"));
+
+		selectDirectory.setOnAction(new EventHandler<ActionEvent>() {
+
+			public void handle(ActionEvent event) {
+				File dir = directoryChooser.showDialog(stage);
+				SitemapGenerator sitemap = new SitemapGenerator("http://northkorea.nk/");
+				BrowseFile fileBrowser = new BrowseFile();
+				if (dir != null) {
+					try {
+						sitemap.execute(dir.getAbsolutePath());
+						fileBrowser.execute(dir.getAbsolutePath());
+					} catch (IOException e) {
+						textArea.setText("Data Corrupted by Capitalism!");
+					}
+				}
+
+			}
+
+		});
+
+		nightMode.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent event) {
+				final URL sceneCSSURL = getClass().getResource("darkTheme.css");
+				stage.setScene(scene);
+				scene.getStylesheets().add(sceneCSSURL.toExternalForm());
+
+			}
+		});
 
 		// When user click on the Exit item.
 		exitItem.setOnAction(new EventHandler<ActionEvent>() {
@@ -145,15 +182,8 @@ public class KimGraphic extends Application {
 
 		readMe.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent event) {
-				FileChooser fileChooser = new FileChooser();
+				File file = new File("README.md");
 
-				// Set extension filter
-				FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("MD FILES (README.md)",
-						"README.md");
-				fileChooser.getExtensionFilters().add(extFilter);
-
-				// Show save file dialog
-				File file = fileChooser.showOpenDialog(stage);
 				if (file != null) {
 					textArea.setText(readFile(file));
 				}
@@ -162,20 +192,23 @@ public class KimGraphic extends Application {
 		});
 
 		// Add menuItems to the Menus
-		fileMenu.getItems().addAll(newItem, exitItem);
-		editMenu.getItems().addAll(xmlOpen);
+		fileMenu.getItems().addAll(nightMode, exitItem);
+		editMenu.getItems().addAll(xmlOpen, selectDirectory);
 		helpMenu.getItems().add(readMe);
 
 		// Add Menus to the MenuBar
 		menuBar.getMenus().addAll(fileMenu, editMenu, helpMenu);
 		root.setPadding(new Insets(0, 10, 20, 10));
-		root.setVgap(50);
+		root.setVgap(20);
 		root.setHgap(20);
-		root.getChildren().add(menuBar);
+		root.getChildren().addAll(menuBar);
 		root.getChildren().addAll(logo, researchField, searchButton, buttonClear);
+		root.getChildren().add(searchArea);
 		root.getChildren().add(scrollPane);
+		final URL sceneCSSURL = getClass().getResource("CssScene.css");
+		stage.setScene(scene);
+		scene.getStylesheets().add(sceneCSSURL.toExternalForm());
 
-		stage.setScene(new Scene(root, 1200, 600));
 		stage.show();
 	}
 
